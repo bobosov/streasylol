1fc1e91975404ff7a575a147ca403d8f50944ade
andrew bobosov <90697184+bobosov@users.noreply.github.com>
2022-11-30 12:21:54 +0300

    Add files via upload

--- stringeas.cpp
+++ stringeas.cpp
@@ -15,12 +15,6 @@ void itc_print_copy_str(string str, int number)
     cout << str;
     }
  }
-void itc_print_copy_char(char str, int number)
-{
-    for (int i = 0; i != number; i ++){
-    cout << str;
-    }
- }
 void itc_first_end_three(string str)
 {
       char a = str[0];
@@ -41,4 +35,40 @@ int itc_count_char_in_str(char ch, string str)
         }
     }
 return count;
+}
+string itc_even_place(string str)
+{
+    return 0;
+}
+double itc_percent_lower_uppercase(string str)
+{
+    return 0;
+}
+string itc_reverse_str(string str)
+{
+    return 0;
+}
+string itc_slice_str(string str, int start, int end)
+{
+    return 0;
+}
+bool itc_equal_reverse(string str)
+{
+    return 0;
+}
+string itc_cmp_str(string str1, string str2, int num)
+{
+    return 0;
+}
+int itc_find_str(string str1, string str2)
+{
+    return 0;
+}
+string itc_three_str(string str1, string str2, string str3)
+{
+    return 0;
+}
+int itc_max_char_on_end(string str)
+{
+    return 0;
 }
